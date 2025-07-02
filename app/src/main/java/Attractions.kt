79317cf94a90a3fad19b44cf3d73b1c33eb7636e
Aanchal Parashar <aanchalparashar936@gmail.com>
2025-07-02 19:34:13 +0530

    Added Attractions.kt model based off the raw JSON file of data. Added view_holder_attraction.xml for use in the UI.

---
+++ app/src/main/java/Attractions.kt
@@ -0,0 +1,9 @@
+data class Attractions(
+    val description: String = "",
+    val facts: List<String> = listOf(),
+    val id: String = "",
+    val image_urls: List<String> = listOf(),
+    val location: Location = Location(),
+    val months_to_visit: String = "",
+    val title: String = ""
+)
---
+++ app/src/main/java/Location.kt
@@ -0,0 +1,4 @@
+data class Location(
+    val latitude: String = "",
+    val longitude: String = ""
+)
