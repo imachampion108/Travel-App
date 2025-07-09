681e1ddcd6f61f4dfa9d8123ba352ef40c78dedc
Aanchal Parashar <aanchalparashar936@gmail.com>
2025-07-09 23:13:22 +0530

    Designed AttractionDetailsFragment.kt file by showing views on its xml file.

--- app/src/main/java/com/example/travelapp/AttractionDetailsFragment.kt
+++ app/src/main/java/com/example/travelapp/AttractionDetailsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.travelapp
 
+import Attractions
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,21 +8,32 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
 import com.example.travelapp.databinding.FragmentAttractionDetailsBinding
+import com.squareup.picasso.Picasso
 
 
-class AttractionDetailsFragment : Fragment() {
+class AttractionDetailsFragment : BaseFragment() {
     private var _binding : FragmentAttractionDetailsBinding? = null
     private val binding get() = _binding!!
     private val safeArgs : AttractionDetailsFragmentArgs by navArgs()
+    private val attraction : Attractions by lazy {
+        attractions.find { it.id == safeArgs.attractionId }!!
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentAttractionDetailsBinding.inflate(inflater,container,false)
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.button.text = safeArgs.attractionId
+        binding.titleTextView.text = attraction.title
+        Picasso.get().load(attraction.image_urls).into(binding.imageHeaderView)
+        binding.monthsToVisitTV.text = attraction.months_to_visit
+        binding.numberOfFactsTextView.text = "${attraction.facts.size} facts"
+        binding.descriptionTV.text = attraction.description
+        binding.numberOfFactsTextView.setOnClickListener {
 
+        }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
