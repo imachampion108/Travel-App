13a95a1f71b9bb5e53d151c7f8de99fc4ec9e126
Aanchal Parashar <aanchalparashar936@gmail.com>
2025-07-13 22:16:53 +0530

    Added background_details_title.xml drawable resource to provide a nice background for the title overlaying on the image. Further connected our nav_graph.xml to the application via `setupActionBarWithNacController()`.

--- app/src/main/java/com/example/travelapp/MainActivity.kt
+++ app/src/main/java/com/example/travelapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.travelapp
 
 import Attractions
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,10 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
@@ -18,13 +23,23 @@ class MainActivity : AppCompatActivity() {
         parseAttractions()
     }
     lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this,navController)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)|| super.onSupportNavigateUp()
+    }
    private fun parseAttractions(): List<Attractions>{
       val textFromFile = resources.openRawResource(R.raw.croatia).bufferedReader().use { it.readText() }
        val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
